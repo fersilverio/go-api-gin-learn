fc72300d18f3387ce1b5c42f7ebfd94b5b7f0bfc
fersilverio <ssilveriofernando45@gmail.com>
2026-09-06 16:10:19 -0300

    first commit

---
+++ usecase/product_usecase.go
@@ -0,0 +1,44 @@
+package usecase
+
+import (
+	"go-api/model"
+	"go-api/repository"
+)
+
+// letra maiuscula para ser visivel fora do pacote
+type ProductUsecase struct {
+	// Repository
+	repository repository.ProductRepository
+}
+
+func NewProductUsecase(repo repository.ProductRepository) ProductUsecase {
+	return ProductUsecase{
+		repository: repo,
+	}
+}
+
+func (pu *ProductUsecase) GetProducts() ([]model.Product, error) {
+	return pu.repository.GetProducts()
+}
+
+func (pu *ProductUsecase) CreateProduct(product model.Product) (model.Product, error) {
+	productId, err := pu.repository.CreateProduct(product)
+
+	if err != nil {
+		return model.Product{}, err
+	}
+
+	product.ID = productId
+
+	return product, nil
+}
+
+func (pu *ProductUsecase) GetProductById(id_product int) (*model.Product, error) {
+
+	product, err := pu.repository.GetProductById(id_product)
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
